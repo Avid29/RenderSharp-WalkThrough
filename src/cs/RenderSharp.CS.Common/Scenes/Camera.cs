95616c4f44d1151a693a0863f7cb96f7c4023023
Adam Dernis <avid29@live.com>
2021-10-27 19:34:08 -0400

    Created Camera

---
+++ src/cs/RenderSharp.CS.Common/Scenes/Camera.cs
@@ -0,0 +1,41 @@
+﻿using System.Numerics;
+
+namespace RenderSharp.CS.Common.Scenes
+{
+    /// <summary>
+    /// A Camera for a RenderSharp scene.
+    /// </summary>
+    public class Camera
+    {
+        /// <summary>
+        /// Creates a new instance of the <see cref="Camera"/> class.
+        /// </summary>
+        /// <param name="origin">The origin of the <see cref="Camera"/>.</param>
+        /// <param name="target">The target position of the <see cref="Camera"/>.</param>
+        /// <param name="fov">The field of view of the <see cref="Camera"/>.</param>
+        public Camera(Vector3 origin, Vector3 target, float fov)
+        {
+            Origin = origin;
+            Target = target;
+            FOV = fov;
+        }
+
+        /// <summary>
+        /// Gets or sets the origin of the <see cref="Camera"/>.
+        /// </summary>
+        public Vector3 Origin { get; set; }
+
+        /// <summary>
+        /// Gets or sets the target position of the <see cref="Camera"/>.
+        /// </summary>
+        /// <remarks>
+        /// Might be replaced with a direction rotation vector.
+        /// </remarks>
+        public Vector3 Target { get; set; }
+
+        /// <summary>
+        /// Gets or sets the field of view for the <see cref="Camera"/>.
+        /// </summary>
+        public float FOV { get; set; }
+    }
+}
