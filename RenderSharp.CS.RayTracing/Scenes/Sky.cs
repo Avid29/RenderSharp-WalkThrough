7ae03fa59d23c18b290d83cf7c29088402a48dee
Adam Dernis <avid29@live.com>
2021-10-29 06:50:32 -0400

    Created RayTracing and Common Sky types

---
+++ RenderSharp.CS.RayTracing/Scenes/Sky.cs
@@ -0,0 +1,30 @@
+﻿using System.Numerics;
+
+namespace RenderSharp.CS.RayTracing.Scenes
+{
+    /// <summary>
+    /// A <see cref="CS.Scenes.Sky"/> for ray tracing.
+    /// </summary>
+    public struct Sky
+    {
+        /// <summary>
+        /// The base color of the sky.
+        /// </summary>
+        public Vector4 albedo;
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="Sky"/>.
+        /// </summary>
+        /// <remarks>
+        /// This is a work around for constructors not working in ComputeSharp shaders.
+        /// </remarks>
+        /// <param name="albedo">The base color of the <see cref="Sky"/>.</param>
+        /// <returns>A new <see cref="Sky"/> with all the specs stated.</returns>
+        public static Sky Create(Vector4 albedo)
+        {
+            Sky sky;
+            sky.albedo = albedo;
+            return sky;
+        }
+    }
+}
---
+++ src/cs/RenderSharp.CS.Common/Scenes/Sky.cs
@@ -0,0 +1,24 @@
+﻿using System.Numerics;
+
+namespace RenderSharp.CS.Scenes
+{
+    /// <summary>
+    /// A Sky for a RenderSharp scene.
+    /// </summary>
+    public class Sky
+    {
+        /// <summary>
+        /// Creates a new instance of the <see cref="Sky"/> class.
+        /// </summary>
+        /// <param name="albedo">The base color of the <see cref="Sky"/>.</param>
+        public Sky(Vector4 albedo)
+        {
+            Albedo = albedo;
+        }
+
+        /// <summary>
+        /// Gets or sets the base color of the <see cref="Sky"/>.
+        /// </summary>
+        public Vector4 Albedo { get; set; }
+    }
+}
