a80531d1254a61dfbb8592ae3855651e1b3dd20c
Adam Dernis <avid29@live.com>
2021-11-01 07:55:18 -0400

    Created IRenderer

---
+++ src/cs/RenderSharp.CS.Common/Render/IRenderer.cs
@@ -0,0 +1,16 @@
+﻿using ComputeSharp;
+
+namespace RenderSharp.CS.Render
+{
+    /// <summary>
+    /// An interface for renderers to render in RenderSharp.CS
+    /// </summary>
+    public interface IRenderer
+    {
+        /// <summary>
+        /// Renders directly to the output buffer.
+        /// </summary>
+        /// <param name="buffer">The output buffer.</param>
+        public void Render(IReadWriteTexture2D<Float4> buffer);
+    }
+}
