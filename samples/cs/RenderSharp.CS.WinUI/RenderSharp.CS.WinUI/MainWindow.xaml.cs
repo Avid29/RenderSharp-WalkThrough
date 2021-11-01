f501fa99edc974fb3033c4fb0bf377de35b76f69
Adam Dernis <avid29@live.com>
2021-11-01 07:56:16 -0400

    Implemented RenderViewer

--- samples/cs/RenderSharp.CS.WinUI/RenderSharp.CS.WinUI/MainWindow.xaml.cs
+++ samples/cs/RenderSharp.CS.WinUI/RenderSharp.CS.WinUI/MainWindow.xaml.cs
@@ -1,20 +1,6 @@
 ﻿using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+using RenderSharp.CS.RayTracing.HLSL;
+using RenderSharp.CS.WinUI.Renderer;
 
 namespace RenderSharp.CS.WinUI
 {
@@ -26,11 +12,11 @@ namespace RenderSharp.CS.WinUI
         public MainWindow()
         {
             this.InitializeComponent();
+
+            HlslRayTraceRenderer renderer = new HlslRayTraceRenderer();
+            Shader = new RenderViewer<HlslRayTraceRenderer>(renderer);
         }
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
-        {
-            myButton.Content = "Clicked";
-        }
+        public RenderViewer<HlslRayTraceRenderer> Shader;
     }
 }
---
+++ samples/cs/RenderSharp.CS.WinUI/RenderSharp.CS.WinUI/Renderer/RenderViewer.cs
@@ -0,0 +1,32 @@
+﻿using ComputeSharp;
+using ComputeSharp.WinUI;
+using RenderSharp.CS.Render;
+using System;
+
+namespace RenderSharp.CS.WinUI.Renderer
+{
+    /// <summary>
+    /// A <see cref="IShaderRunner"/> for displaying <see cref="IRenderer"/>s.
+    /// </summary>
+    /// <typeparam name="TRenderer">The type of <see cref="IRenderer"/> to show.</typeparam>
+    public class RenderViewer<TRenderer> : IShaderRunner
+        where TRenderer : IRenderer
+    {
+        private TRenderer _renderer;
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="RenderViewer{TRenderer}"/> class.
+        /// </summary>
+        /// <param name="renderer">The rendere<see cref="IRenderer"/> being viewed.</param>
+        public RenderViewer(TRenderer renderer)
+        {
+            _renderer = renderer;
+        }
+
+        /// <inheritdoc/>
+        public void Execute(IReadWriteTexture2D<Float4> texture, TimeSpan timespan)
+        {
+            _renderer.Render(texture);
+        }
+    }
+}
